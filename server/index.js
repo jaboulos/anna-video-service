20a7461f467bdf0a1c576e2f29b0b81a1125c5c7
Faris Habib <faris.habib.7@gmail.com>
2019-01-05 10:18:19 -0800

    Creates models and collections to query db and build api routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
 const db = require('../database/config.js');
+const Videos = require('../database/collections/videos.js');
+const Video = require('../database/models/video.js');
+const Users = require('../database/collections/users.js');
+const User = require('../database/models/user.js');
 const app = express();
 const port = 3049;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true, }));
 app.use(express.static(path.join(__dirname, '../public')));
+
+
 
 app.get('/', (req, res) => {
   console.log('Made it');
