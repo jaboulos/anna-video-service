22e2434bb8ba2161e0120d06093a7c43120b3c60
Faris Habib <faris.habib.7@gmail.com>
2019-01-06 20:37:12 -0800

    adds VideoCollection component for rendering static thumbnail images of all videos in database.

--- client/VideoCollection.jsx
+++ client/VideoCollection.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
-/*
+const VideoCollection = (props) => {
+  <div className="preview-card">
+    {props.videos.map((video) => {
+      return (
+        <li>
+          <div>{video.thumbnail_url}</div>
+          <div>{video.title}</div>
+          <div>{video.user_name}</div>
+          <div>Game title goes here</div>
+          <div>Duration: {video.duration}</div>
+        </li>
+      );
+    })}
+  </div>;
+};
 
-  TODO:
-
-    -Complete Video Collection Component
-*/
+export default VideoColleciton;
--- client/index.jsx
+++ client/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import VideoPlayer from './VideoPlayer.jsx';
+import VideoCollection from './VideoCollection.jsx';
 
 class App extends React.Component {
   constructor(props) {
