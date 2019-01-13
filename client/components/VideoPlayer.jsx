d65ac099130f7d0e1698dbd97c7991213cf705e7
Faris Habib <faris.habib.7@gmail.com>
2019-01-12 23:21:09 -0800

    Styled Video Player component w/ styled components

--- client/components/VideoPlayer.jsx
+++ client/components/VideoPlayer.jsx
@@ -1,25 +1,76 @@
 import React from 'react';
 import moment from 'moment';
+import styled from 'styled-components';
+
+const VideoPlayerStyle = styled.div`
+  display: inline-block;
+  width: 1080px;
+  borderStyle: solid;
+  borderWidth: 1px;
+  text-align: left;
+  vertical-align: top;
+  font-size: 20px;
+  font-weight: 350;
+  background-color: #faf9fa;
+  box-shadow: 5px 5px 2px 1px #dad8de;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+`;
+
+const CategoryStyle = styled.span`
+  font-size: 15px;
+`;
+
+const ImageStyle = styled.div`
+  float: left;
+  padding-right: 10px;
+`;
+
+const ViewStyle = styled.div`
+  float: right;
+  text-align: right;
+  color: #999999;
+`;
+
+const GameNameStyle = styled.span`
+  padding-left: 2px;
+  font-size: 15px;
+  color: #6441a4;
+  cursor: pointer;
+  &: hover {
+    text-decoration: underline;
+  }
+`;
 
 const VideoPlayer = (props) => {
   return (
-    <div className="video-player">
-      <div className="embed-responsive embed-responsive-16by9">
-        <iframe className="embed-responsive-item"
-          src={'https://player.twitch.tv/?video=v' + props.video.id}
-          height="650"
-          width="1080"
-          frameBorder="5"
-          scrolling="no"
-          allowFullScreen="true">
-        </iframe>
-      </div>
-      <div className="video-player-details">
-        <div><img src={props.game.box_art_url}/></div>
-        <h3>{props.video.title} · {moment(props.video.created_at).fromNow()}</h3>
-        <div>Category: {props.game.name}</div>
-        <div>Views: {props.video.view_count}</div>
-      </div>
+    <div>
+      <iframe className="embed-responsive-item"
+        src={'https://player.twitch.tv/?video=v' + props.video.id}
+        height="600"
+        width="1075"
+        scrolling="no"
+        allowFullScreen={true}>
+      </iframe>
+      <VideoPlayerStyle>
+        <ImageStyle><img src={props.game.box_art_url}/></ImageStyle>
+        <p>{props.video.title} · {moment(props.video.created_at).fromNow()}</p>
+        <ViewStyle>
+          <svg width="5%" height="5%" viewBox="0 0 20 20">
+            <path
+              d="M10 15c-4.044 0-7-3.288-7-5 0-1.712 2.956-5 7-5 4.044 0 7 3.288 7 5 0 1.712-2.956 5-7
+            5m0-12c4.763 0 9 3.914 9 7s-4.237 7-9 7-9-3.914-9-7 4.237-7 9-7zm3 7a3 3 0 1 0-6 0 3 3 0 0 0 6 0z"
+              fill="#999999"
+              fill-rule="evenodd">
+            </path>
+          </svg>
+          <span> {props.video.view_count}</span>
+        </ViewStyle>
+        <div>
+          <CategoryStyle>Category:
+            <GameNameStyle>{props.game.name}</GameNameStyle>
+          </CategoryStyle>
+        </div>
+      </VideoPlayerStyle>
     </div>
   );
 };
