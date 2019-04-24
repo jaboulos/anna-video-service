f951d4af7992a66b22e2e90e60de04a7d5ec4834
Joe Boulos <jab12288@gmail.com>
2019-04-24 14:14:47 -0700

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,50 @@
-const webpack = require('webpack');
-const path = require('path');
+// const webpack = require('webpack');
+// const path = require('path');
+
+// module.exports = {
+//   context: __dirname + '/client',
+//   entry: './index.jsx',
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader'
+//         }
+//       }
+//     ]
+//   },
+//   output: {
+//     path: __dirname + '/public',
+//     filename: 'bundle.js',
+//   }
+// };
+
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client');
+var DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
-  context: __dirname + '/client',
-  entry: './index.jsx',
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/react', '@babel/env']
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
-  },
-  output: {
-    path: __dirname + '/public',
-    filename: 'bundle.js',
   }
 };
